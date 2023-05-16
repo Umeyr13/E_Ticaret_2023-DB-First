62b2b772a30412915737cad11aac7f5c49f7dfbe
umeyr13 <umeyr.gulcimen@gmail.com>
2023-05-16 14:27:35 +0300

    Add project files.

---
+++ E_Ticaret_2023/Controllers/KategoriController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using E_Ticaret_2023.Models;
+
+namespace E_Ticaret_2023.Controllers
+{
+    public class KategoriController : Controller
+    {
+        private E_Ticaret_2023Entities1 db = new E_Ticaret_2023Entities1();
+
+        // GET: Kategori
+        public ActionResult Index()
+        {
+            return View(db.Kategoriler.ToList());
+        }
+
+        // GET: Kategori/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategoriler kategoriler = db.Kategoriler.Find(id);
+            if (kategoriler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategoriler);
+        }
+
+        // GET: Kategori/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Kategori/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "KategoriId,KategoriAdi")] Kategoriler kategoriler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Kategoriler.Add(kategoriler);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(kategoriler);
+        }
+
+        // GET: Kategori/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategoriler kategoriler = db.Kategoriler.Find(id);
+            if (kategoriler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategoriler);
+        }
+
+        // POST: Kategori/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "KategoriId,KategoriAdi")] Kategoriler kategoriler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kategoriler).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kategoriler);
+        }
+
+        // GET: Kategori/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategoriler kategoriler = db.Kategoriler.Find(id);
+            if (kategoriler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategoriler);
+        }
+
+        // POST: Kategori/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Kategoriler kategoriler = db.Kategoriler.Find(id);
+            db.Kategoriler.Remove(kategoriler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
